b1e1bc15b5f51a611a00a2f234b0d5d758eeffb1
Biswadeep Roy <74821633+biswadeep-roy@users.noreply.github.com>
2023-12-24 21:08:24 +0530

    Update register.js
    please check the code and merge it

--- src/components/register.js
+++ src/components/register.js
@@ -1,3 +1,16 @@
+import React, { useState, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import {
+  MDBContainer,
+  MDBCol,
+  MDBRow,
+  MDBInput,
+  MDBCard,
+  MDBCardBody,
+  MDBCardImage,
+} from 'mdb-react-ui-kit';
+
+
 import React from 'react'
 
 const register = () => {
